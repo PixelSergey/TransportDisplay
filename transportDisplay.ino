bdbeb79ccce3041b399a9c01431b4e429bcba7d8
PixelSergey <sergey.ichtchenko@gmail.com>
2020-04-12 13:58:00 +0300

    Change JSON library to ArduinoJson

--- transportDisplay.ino
+++ transportDisplay.ino
@@ -22,14 +22,13 @@
 #include <LiquidCrystal_I2C.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include "rapidjson/document.h"
+#include <ArduinoJson.h>
 
 // NodeMCU uses 0x3F as the address
 // Set screen to width 16 and height 2
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 HTTPClient http;
 WiFiClient wclient;
-rapidjson::Document doc;
 
 // Request text easily converted to this format using https://tomeko.net/online_tools/cpp_text_escape.php?lang=en
 char request[] =
@@ -100,24 +99,37 @@ void setup(){
     lcd.print("Received data!");
     lcd.setCursor(0, 1);
     lcd.print("Check serial");
-    doc.Parse(json_str);
-    const Value& data = doc["data"];
-    const Value& stop = data["stop"];
-    String name = stop["name"].GetString();
-    Serial.println(name);
-    const Value& stoptimesWithoutPatterns = stop["stoptimesWithoutPatterns"];
-    int serviceDay = stoptimesWithoutPatterns[0]["serviceDay"].GetInt();
-    Serial.println(serviceDay); 
-    int scheduledArrival = stoptimesWithoutPatterns[0]["scheduledArrival"].GetInt();
-    Serial.println(scheduledArrival);
-    int realtimeArrival = stoptimesWithoutPatterns[0]["realtimeArrival"].GetInt();
-    Serial.println(realtimeArrival);
-    const Value& trip = stoptimesWithoutPatterns[0]["trip"];
-    const Value& route = trip["route"];
-    Serial.println(route["shortName"].GetString());
 
+    const size_t capacity = JSON_ARRAY_SIZE(1) + 4*JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(6) + 200;
+    DynamicJsonDocument doc(capacity);
+    const char* json = payload.c_str();
+    deserializeJson(doc, json);
+    JsonObject data_stop = doc["data"]["stop"];    
+    JsonObject times = data_stop["stoptimesWithoutPatterns"][0];
+    long serviceDay = times["serviceDay"]; // 1586638800
+    long scheduledArrival = times["scheduledArrival"]; // 41520
+    long scheduledDeparture = times["scheduledDeparture"]; // 41520
+    long realtimeArrival = times["realtimeArrival"]; // 41520
+    long realtimeDeparture = times["realtimeDeparture"]; // 41520
+    const char* route = times["trip"]["route"]["shortName"]; // "10"
+    Serial.println(serviceDay);
+    Serial.println(scheduledArrival);
+    Serial.println(scheduledDeparture);
+    Serial.println(realtimeArrival);
+    Serial.println(realtimeDeparture);
+    Serial.println(route);
 }
 
 void loop(){
-    
+    /*
+    http.begin(wclient, "http://worldtimeapi.org/api/timezone");
+    //http.addHeader("Accept", "application/json");
+    //http.addHeader("Accept-Charset", "utf-8");
+    int httpCode = http.GET();
+    String payload = http.getString();
+    Serial.println(httpCode);
+    Serial.println(payload);
+    http.end();
+    delay(3000);
+    */
 }
